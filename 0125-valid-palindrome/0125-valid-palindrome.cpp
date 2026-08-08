721220b38802ec198b51260861a7686c72ca9093
Ravi Pushp <148735581+ravipushp@users.noreply.github.com>
2026-08-08 12:40:58 +0530

    Time: 0 ms (100%), Space: 10.3 MB (15.25%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string result = "";
+        for(char c : s) {
+            if(isalnum(c)) {
+                result += tolower(c);
+            }
+        }
+        int left=0;
+        int right=result.length()-1;
+        while(left<right){
+            if(result[left]!=result[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};

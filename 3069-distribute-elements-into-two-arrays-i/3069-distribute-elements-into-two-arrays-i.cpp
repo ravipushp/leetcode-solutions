26a42b8faf95cfae012822e818c8bd421163da7e
Ravi Pushp <148735581+ravipushp@users.noreply.github.com>
2026-08-20 23:17:01 +0530

    Time: 3 ms (23.77%), Space: 24 MB (45.52%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -5,23 +5,20 @@ public:
         vector<int> arr2;
         arr1.push_back(nums[0]);
         arr2.push_back(nums[1]);
-        for(int i=2;i<=nums.size()-1;i++){
+        for(int i=2;i<nums.size();i++){
             if(arr1.back() > arr2.back()){
                 arr1.push_back(nums[i]);
             }else{
                 arr2.push_back(nums[i]);
             }
         }
-         vector<int> result;
-
+        vector<int> result;
         for(int x : arr1) {
             result.push_back(x);
         }
-
         for(int x : arr2) {
             result.push_back(x);
         }
-
         return result;
 
     }
